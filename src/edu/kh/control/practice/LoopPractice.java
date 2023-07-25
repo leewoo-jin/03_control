78227352f88d63a51994bb669ce7c239ef10da31
leewoo-jin <fkaustkwnfrk@gmail.com>
2023-07-26 08:54:10 +0900

    20230726

---
+++ src/edu/kh/control/practice/LoopPractice.java
@@ -0,0 +1,17 @@
+package edu.kh.control.practice;
+
+public class LoopPractice {
+
+	     public void practice1() {
+	    	 
+	    	 for(int i = 1; i <= 4; i++) {
+	    		 
+	    		 
+	    		 
+	    	 }
+	    	 
+	     }
+		
+	}
+
+
---
+++ src/edu/kh/control/practice/Run2.java
@@ -0,0 +1,19 @@
+package edu.kh.control.practice;
+
+public class Run2 {
+
+	public static void main(String[] args) {
+		
+		LoopPractice LP = new LoopPractice();
+		
+		LP.practice1();
+		
+		
+				
+		
+		
+		
+
+	}
+
+}
